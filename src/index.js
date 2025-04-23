9bc9d8b28bce2ca83277d41f50117628822b026e
fawuzantech <ibrahimpima76@gmail.com>
2025-04-23 23:56:25 +0000

    feat: add initial express server setup with quotes router
    This commit introduces the basic setup for an Exp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const quotesRouter = require('./routes/quotes');
+const quotesRouter = require('../routes/quotes');
 
 const app = express();
 
